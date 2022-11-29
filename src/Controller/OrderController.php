7a419161591faea139e23e15ecc31731ef232014
aniskraiem <anis.kraiem1996@gmail.com>
2022-11-30 00:16:24 +0100

    Update OrderController.php

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -20,14 +20,14 @@ use App\Service\ApiService;
 
 class OrderController extends AbstractController
 {
-    
+
     /**
      * @Route("/orders", name="orders", methods={"GET"})
      */
 
     public function fetchOrders(ApiService $ApiService): Response
     {
-//        dd($ApiService->getOrders());
+        //        dd($ApiService->getOrders());
         $response = $this->render('order.html.twig', [
             'data' => $ApiService->getOrders(),
         ]);
@@ -37,96 +37,91 @@ class OrderController extends AbstractController
         return $response;
     }
 
- /**
+    /**
      * @Route("/flow/orders_to_csv", name="OrdersToCsv")
      */
     public function orderstocsv(ApiService $ApiService): Response
     {
-    //Nom des colonnes en première lignes
-    // le \n à la fin permets de faire un saut de ligne, super important en CSV
-    // le point virgule sépare les données en colonnes
+        //Nom des colonnes en première lignes
+        // le \n à la fin permets de faire un saut de ligne, super important en CSV
+        // le point virgule sépare les données en colonnes
 
-//    $myVariableCSV = "order:  delivery_name: delivery_address:  delivery_country:  delivery_zipcode:  delivery_city:  items_count:  item_index:  item_id:  item_quantity:  line_price_excl_vat:  line_price_incl_vat:\n";
-    
+        //    $myVariableCSV = "order:  delivery_name: delivery_address:  delivery_country:  delivery_zipcode:  delivery_city:  items_count:  item_index:  item_id:  item_quantity:  line_price_excl_vat:  line_price_incl_vat:\n";
 
-//Ajout de données (avec le . devant pour ajouter les données à la variable existante)
-    $myVariableCSV = " ";
 
-    $orders  = $ApiService->getOrders();
-    $contacts  = $ApiService->getContacts();
+        //Ajout de données (avec le . devant pour ajouter les données à la variable existante)
+        $myVariableCSV = " ";
 
-    foreach ($orders['results'] as $o) {
-        $myVariableCSV .= "\n order: ";
+        $orders = $ApiService->getOrders();
+        $contacts = $ApiService->getContacts();
 
-            $myVariableCSV .=  $o['OrderNumber'];
-           
+        foreach ($orders['results'] as $o) {
+            $myVariableCSV .= "\n order: ";
 
-        foreach ($contacts['results'] as $c) {
-            
+            $myVariableCSV .= $o['OrderNumber'];
 
-if($c['ID']==$o['DeliverTo']){
 
-        $myVariableCSV .= "\n delivery_name: ";
+            foreach ($contacts['results'] as $c) {
 
-        $myVariableCSV .=  $c['AccountName'];
-        $myVariableCSV .= "\n delivery_address: ";
 
-        $myVariableCSV .=  $c['AddressLine1'];
-        $myVariableCSV .= "\n delivery_country: ";
+                if ($c['ID'] == $o['DeliverTo']) {
 
-        $myVariableCSV .=  $c['Country'];
+                    $myVariableCSV .= "\n delivery_name: ";
+                    $myVariableCSV .= $c['AccountName'];
 
-        $myVariableCSV .= "\n delivery_zipcode: ";
+                    $myVariableCSV .= "\n delivery_address: ";
+                    $myVariableCSV .= $c['AddressLine1'];
 
-        $myVariableCSV .=  $c['ZipCode'];
-        $myVariableCSV .= "\n delivery_city: ";
+                    $myVariableCSV .= "\n delivery_country: ";
+                    $myVariableCSV .= $c['Country'];
 
-        $myVariableCSV .=  $c['City'];
-        $myVariableCSV .= "\n item_count: ";
+                    $myVariableCSV .= "\n delivery_zipcode: ";
+                    $myVariableCSV .= $c['ZipCode'];
 
-        $myVariableCSV .=  count($o['SalesOrderLines']['results']);
-   
-         foreach ($o['SalesOrderLines']['results'] as $s) {
-        
-        $myVariableCSV .= "\n item_index: ";
+                    $myVariableCSV .= "\n delivery_city: ";
+                    $myVariableCSV .= $c['City'];
 
-        $myVariableCSV .=  $s['Description'];
-        $myVariableCSV .= "\n item_id: ";
+                    $myVariableCSV .= "\n item_count: ";
+                    $myVariableCSV .= count($o['SalesOrderLines']['results']);
 
-        $myVariableCSV .=  $s['Item'];
-        $myVariableCSV .= "\n item_quantity: ";
+                    foreach ($o['SalesOrderLines']['results'] as $s) {
 
-        $myVariableCSV .=  $s['Quantity'];
-        $myVariableCSV .= "\n line_price_excl_vat: ";
+                        $myVariableCSV .= "\n item_index: ";
+                        $myVariableCSV .= $s['Description'];
 
-        $myVariableCSV .=  $s['Amount'];
-        $myVariableCSV .= "\n line_price_incl_vat: ";
+                        $myVariableCSV .= "\n item_id: ";
+                        $myVariableCSV .= $s['Item'];
 
-        $myVariableCSV .=  $s['Amount']+ $s['VATAmount'];
-        
+                        $myVariableCSV .= "\n item_quantity: ";
+                        $myVariableCSV .= $s['Quantity'];
+
+                        $myVariableCSV .= "\n line_price_excl_vat: ";
+                        $myVariableCSV .= $s['Amount'];
+
+                        $myVariableCSV .= "\n line_price_incl_vat: ";
+                        $myVariableCSV .= $s['Amount'] + $s['VATAmount'];
+
+                    }
+                }
+            }
+
+        }
+
+        //Si l'on souhaite ajouter un espace
+
+        //On donne la variable en string à la response, nous définissons le code HTTP à 200
+        return new Response(
+            $myVariableCSV,
+            200,
+            [
+                //Définit le contenu de la requête en tant que fichier Excel
+                'Content-Type' => 'application/vnd.ms-excel',
+                //On indique que le fichier sera en attachment donc ouverture de boite de téléchargement ainsi que le nom du fichier
+                "Content-disposition" => "attachment; filename=orders.csv"
+            ],
+
+        );
     }
-}
-    }
- 
-    }
-    
-    //Si l'on souhaite ajouter un espace
- 
-    //On donne la variable en string à la response, nous définissons le code HTTP à 200
-    return new Response(
-        
-           $myVariableCSV,
-           200,
-           
-           [
-         //Définit le contenu de la requête en tant que fichier Excel
-             'Content-Type' => 'application/vnd.ms-excel',
-         //On indique que le fichier sera en attachment donc ouverture de boite de téléchargement ainsi que le nom du fichier
-             "Content-disposition" => "attachment; filename=orders.csv"
-           ],
-          
-    );
-    }
-   
+
 
 }
